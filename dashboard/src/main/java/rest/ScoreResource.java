05f1438f15cd4471e50598eefb086b8dc1aba7b7
xstefank <xstefank122@gmail.com>
2024-06-03 18:50:59 +0200

    Database

---
+++ dashboard/src/main/java/rest/ScoreResource.java
@@ -0,0 +1,30 @@
+package rest;
+
+import entity.Score;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import service.ScoreService;
+
+import java.util.List;
+
+@Path("/score")
+public class ScoreResource {
+
+    @Inject
+    ScoreService scoreService;
+
+    @GET
+    public List<Score> all() {
+        return Score.listAll();
+    }
+
+    @GET
+    @Path("/average")
+    public List<ScoreService.AverageScore> average() {
+        return scoreService.averageScores();
+    }
+
+
+
+}
